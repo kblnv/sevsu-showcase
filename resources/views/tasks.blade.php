3d29d3310b48abba693be68340a966d131d42c45
Kabalinov Maxim <maximdipll8@gmail.com>
2024-03-24 20:58:11 +0300

    Добавлен select на страницу банка задач

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,5 +5,16 @@
 @endsection
 
 @section("content")
-  <h1 class="text-xl">Банк задач</h1>
+  <label class="block" for="flow">Выберите поток</label>
+  <select
+    class="mt-2 bg-sevsu-light-gray border-2 border-gray-300 rounded-lg outline-none focus:ring-sevsu-blue focus:border-sevsu-blue p-2.5"
+    id="flow"
+  >
+    <option value="" disabled>Поток</option>
+    <option value="Веб-технологии РГР">Веб-технологии РГР</option>
+    <option value="Проектирование в профессиональной сфере">
+      Проектирование в профессиональной сфере
+    </option>
+    <option value="Курсовой проект">Курсовой проект</option>
+  </select>
 @endsection
