5bae4656ac822b258c34f43d4bb61f4206a2d1b0
Kabalinov Maxim <maximdipll8@gmail.com>
2024-04-13 14:34:50 +0300

    Миграция на Volt

--- resources/views/livewire/my-teams-page.blade.php
+++ resources/views/livewire/my-teams-page.blade.php
@@ -1,27 +1,149 @@
-<div>
-  @if (count($this->flows) == 0 || !$this->hasTeams())
-    <x-shared.page-heading>
-      Вы не состоите ни в одной команде
-    </x-shared.page-heading>
-  @else
-    <x-shared.page-heading>
-      Все команды, в которых Вы состоите:
-    </x-shared.page-heading>
+<?php
 
-    <div class="mt-4 space-y-8">
-      @foreach ($this->flows as $flow => $params)
-        @if (! empty($params))
-          <x-entities.team-card
-            :title="$params['team']['title']"
-            :task="$params['team']['task']"
-            :flow="$flow"
-            :description="$params['team']['description']"
-            :maxTeamMembers="$params['maxTeamMembers']"
-            :tags="$params['team']['tags']"
-            :members="$params['team']['members']"
-          />
-        @endif
-      @endforeach
-    </div>
+use Livewire\Volt\Component;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Computed;
+
+new
+#[Title('Мои команды')]
+class extends Component {
+    #[Computed(persist: true, seconds: 300)]
+    public function flows()
+    {
+        return [
+            "Дисциплина 1" => [
+                "maxTeamMembers" => "10",
+                "team" => [
+                    "title" => "Команда дисциплины 1",
+                    "task" => "Задача команды дисциплины 1",
+                    "description" => "Описание команады дисциплины 1",
+                    "tags" => ['Веб-программирование', 'Профессиональный трек'],
+                    "members" => [
+                        [
+                            "fullName" => "Кабалинов Максим Владимирович",
+                            "vacancy" => "Front-end разработчик",
+                            "isModerator" => true
+                        ],
+                        [
+                            "fullName" => "Аракелян Сурен Рубенович",
+                            "vacancy" => "Back-end разработчик",
+                            "isModerator" => false
+                        ],
+                        [
+                            "fullName" => "Семенов Валентин Андреевич",
+                            "vacancy" => "Back-end разработчик",
+                            "isModerator" => false
+                        ],
+                        [
+                            "fullName" => "Остапович Андрей Викторович",
+                            "vacancy" => "Front-end разработчик",
+                            "isModerator" => false
+                        ],
+                    ]
+                ],
+            ],
+            "Дисциплина 2" => [
+                "maxTeamMembers" => "10",
+                "team" => [
+                    "title" => "Команда дисциплины 2",
+                    "task" => "Задача команды дисциплины 2",
+                    "description" => "Описание команады дисциплины 2",
+                    "tags" => ['Веб-программирование', 'Профессиональный трек'],
+                    "members" => [
+                        [
+                            "fullName" => "Кабалинов Максим Владимирович",
+                            "vacancy" => "Front-end разработчик",
+                            "isModerator" => true
+                        ],
+                        [
+                            "fullName" => "Аракелян Сурен Рубенович",
+                            "vacancy" => "Back-end разработчик",
+                            "isModerator" => false
+                        ],
+                        [
+                            "fullName" => "Семенов Валентин Андреевич",
+                            "vacancy" => "Back-end разработчик",
+                            "isModerator" => false
+                        ],
+                        [
+                            "fullName" => "Остапович Андрей Викторович",
+                            "vacancy" => "Front-end разработчик",
+                            "isModerator" => false
+                        ],
+                    ]
+                ]
+            ],
+            "Дисциплина 3" => [
+                "maxTeamMembers" => "10",
+                "team" => [
+                    "title" => "Команда дисциплины 3",
+                    "task" => "Задача команды дисциплины 3",
+                    "description" => "Описание команады дисциплины 3",
+                    "tags" => ['Веб-программирование', 'Профессиональный трек'],
+                    "members" => [
+                        [
+                            "fullName" => "Кабалинов Максим Владимирович",
+                            "vacancy" => "Front-end разработчик",
+                            "isModerator" => true
+                        ],
+                        [
+                            "fullName" => "Аракелян Сурен Рубенович",
+                            "vacancy" => "Back-end разработчик",
+                            "isModerator" => false
+                        ],
+                        [
+                            "fullName" => "Семенов Валентин Андреевич",
+                            "vacancy" => "Back-end разработчик",
+                            "isModerator" => false
+                        ],
+                        [
+                            "fullName" => "Остапович Андрей Викторович",
+                            "vacancy" => "Front-end разработчик",
+                            "isModerator" => false
+                        ],
+                    ]
+                ]
+            ]
+        ];
+    }
+
+    public function hasTeams()
+    {
+        $hasTeams = false;
+        foreach ($this->flows as $flow => $params) {
+            if (! empty($params)) {
+                $hasTeams = true;
+            }
+        }
+        return $hasTeams;
+    }
+
+}; ?>
+
+<div>
+    @if (count($this->flows) == 0 || ! $this->hasTeams())
+        <x-shared.page-heading>
+            Вы не состоите ни в одной команде
+        </x-shared.page-heading>
+    @else
+        <x-shared.page-heading>
+            Все команды, в которых Вы состоите:
+        </x-shared.page-heading>
+
+        <div class="mt-4 space-y-8">
+            @foreach ($this->flows as $flow => $params)
+                @if (! empty($params))
+                    <x-entities.team-card
+                        :title="$params['team']['title']"
+                        :task="$params['team']['task']"
+                        :flow="$flow"
+                        :description="$params['team']['description']"
+                        :maxTeamMembers="$params['maxTeamMembers']"
+                        :tags="$params['team']['tags']"
+                        :members="$params['team']['members']"
+                    />
+                @endif
+            @endforeach
+        </div>
   @endif
 </div>
--- resources/views/livewire/teams-page.blade.php
+++ resources/views/livewire/teams-page.blade.php
@@ -1,46 +1,144 @@
+<?php
+
+use Livewire\Volt\Component;
+use App\Models\Flow;
+use App\Models\TagTask;
+use App\Models\Team;
+use App\Models\UserTeam;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
+use Livewire\Attributes\Computed;
+use Livewire\WithPagination;
+
+new
+#[Title('Команды')]
+class extends Component {
+    use WithPagination;
+
+    #[Url]
+    public $selectedFlow = "";
+
+    #[Computed(persist: true, seconds: 300)]
+    public function flows()
+    {
+        $user = auth()->user();
+
+        $flows = Flow::select(
+                'flows.id',
+                'flows.flow_name',
+                'flows.max_team_size',
+            )
+            ->join('groups_flows', 'flows.id', '=', 'groups_flows.flow_id')
+            ->where('groups_flows.group_id', $user->group_id)
+            ->get();
+
+        return $flows;
+    }
+
+    public function teams()
+    {
+        $user = auth()->user();
+
+        $teams = Team::select(
+                'teams.id',
+                'teams.team_name',
+                'teams.team_description',
+                'tasks.id',
+                'tasks.task_name',
+                'tasks.task_description',
+            )
+            ->join('tasks', 'teams.task_id', '=', 'tasks.id')
+            ->join('flows', 'tasks.flow_id', '=', 'flows.id')
+            ->where('flows.flow_name', '=', $this->selectedFlow)
+            ->paginate(10);
+
+        return $teams;
+    }
+
+    public function members($teamId)
+    {
+        $members = UserTeam::select(
+            'users.first_name',
+            'users.second_name',
+            'users.last_name',
+            'users_teams.is_moderator',
+        )
+        ->join('users', 'users_teams.user_id', '=', 'users.id')
+        ->join('teams', 'teams.id', '=', 'users_teams.team_id')
+        ->where('teams.id', '=', $teamId)
+        ->join('vacancies', 'users.id', '=', 'vacancies.user_id')
+        ->where('vacancies.team_id', '=', $teamId)
+        ->get();
+
+        return $members;
+    }
+
+    public function tags($taskId)
+    {
+        $tags = TagTask::select('tags.tag_name')
+            ->join('tags', 'tags_tasks.tag_id', '=', 'tags.id')
+            ->where('tags_tasks.task_id', '=', $taskId)
+            ->pluck('tag_name')
+            ->toArray();
+
+        return $tags;
+    }
+
+    public function mount()
+    {
+        if ($this->selectedFlow == "" || !$this->flows()->firstWhere('flow_name', $this->selectedFlow)) {
+            $this->selectedFlow = $this->flows()->first()->flow_name ?? '';
+        }
+    }
+}; ?>
+
 <div>
-  @if ($selectedFlow == "")
-    <x-shared.page-heading>
-      Вы не прикреплены ни к одной дисциплине
-    </x-shared.page-heading>
-  @else
-    <x-shared.select
-      id="flow"
-      label="Выберите дисциплину для отображения:"
-      wire:model.live="selectedFlow"
-      wire:change="setPage(1)"
-    >
-      <option value="" disabled>Дисциплина</option>
-      @foreach ($this->flows as $flow)
-        @if ($flow->flow_name == $selectedFlow)
-          <option value="{{ $flow->flow_name }}" selected>{{ $flow->flow_name }}</option>
-        @else
-          <option value="{{ $flow->flow_name }}">{{ $flow->flow_name }}</option>
-        @endif
-      @endforeach
-    </x-shared.select>
-
-    @if ($this->flows->count() == 0)
-      <x-shared.page-heading class="mt-8">
-        Нет задач по выбранной дисциплине
-      </x-shared.page-heading>
+    @if ($selectedFlow == "")
+        <x-shared.page-heading>
+            Вы не прикреплены ни к одной дисциплине
+        </x-shared.page-heading>
     @else
-      <x-shared.page-heading class="mt-8">
-        Банк задач по выбранной дисциплине:
-      </x-shared.page-heading>
+        <x-shared.select
+            id="flow"
+            label="Выберите дисциплину для отображения:"
+            wire:model.live="selectedFlow"
+            wire:change="setPage(1)"
+        >
+            <option value="" disabled>Дисциплина</option>
+            @foreach ($this->flows as $flow)
+                @if ($flow->flow_name == $selectedFlow)
+                    <option value="{{ $flow->flow_name }}" selected>
+                        {{ $flow->flow_name }}
+                    </option>
+                @else
+                    <option value="{{ $flow->flow_name }}">
+                        {{ $flow->flow_name }}
+                    </option>
+                @endif
+            @endforeach
+        </x-shared.select>
 
-      <div class="mt-4 space-y-8">
-        @foreach ($this->teams()->items() as $team)
-          <x-entities.task-card
-            :title="$team['team_name']"
-            :description="$team['team_description']"
-            :maxTeamMembers="$this->flows->firstWhere('flow_name', $selectedFlow)->max_team_size"
-            :tags="$this->tags($team['tasks_id'])"
-          />
-        @endforeach
-      </div>
+        @if ($this->flows->count() == 0)
+            <x-shared.page-heading class="mt-8">
+                Нет задач по выбранной дисциплине
+            </x-shared.page-heading>
+        @else
+            <x-shared.page-heading class="mt-8">
+                Банк задач по выбранной дисциплине:
+            </x-shared.page-heading>
+
+            <div class="mt-4 space-y-8">
+                @foreach ($this->teams()->items() as $team)
+                    <x-entities.task-card
+                        :title="$team['team_name']"
+                        :description="$team['team_description']"
+                        :maxTeamMembers="$this->flows->firstWhere('flow_name', $selectedFlow)->max_team_size"
+                        :tags="$this->tags($team['tasks_id'])"
+                    />
+                @endforeach
+            </div>
+        @endif
+
+        {{ $this->teams()->links() }}
     @endif
-
-    {{ $this->teams()->links() }}
-  @endif
 </div>
