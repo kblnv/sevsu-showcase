89eb50ed630d407039ff31cce3c15e63346d0250
SkofiTI <cactus.master994@gmail.com>
2024-05-30 00:10:31 +0300

    Добавлена библиотека Spatie. Добавлена админ-панель Filament. Добавлено разделение ролей пользователей

---
+++ app/Filament/Resources/FlowResource/Pages/CreateFlow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Resources\FlowResource\Pages;
+
+use App\Filament\Resources\FlowResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateFlow extends CreateRecord
+{
+    protected static string $resource = FlowResource::class;
+    protected static ?string $title = 'Добавить Дисциплину';
+}
