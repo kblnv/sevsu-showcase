1327f03117405dc342dded71dd30071bd26fbd9f
Andrey Ostapovich <artvik48@gmail.com>
2024-05-31 18:14:34 +0300

    Рефакторинг компонента табов

--- resources/views/livewire/pages/tasks/show.blade.php
+++ resources/views/livewire/pages/tasks/show.blade.php
@@ -15,11 +15,13 @@ new #[Title("Задача")] class extends Component {
     public $teams;
     public $canCreateTeam;
 
+    // TODO: move to mount()
     #[Url]
     public $currentTab = "";
-    public $availableTabs = ["Задача", "Команды", "Создание команды"];
+    public $defaultTab = "Задача";
+    public $tabs = ["Задача", "Команды", "Создание команды"];
 
-    public function selectTab($tabName)
+    public function switchTab($tabName)
     {
         $this->currentTab = $tabName;
     }
@@ -37,8 +39,13 @@ new #[Title("Задача")] class extends Component {
         $this->teams = Teams::getTeamsByTask($task["id"]);
         $this->canCreateTeam = Teams::canCreateTeam($this->taskId, Auth::id());
 
-        if ($this->currentTab === "" || !in_array($this->currentTab, $this->availableTabs) || ($this->currentTab === "Создание команды" && !$this->canCreateTeam)) {
-            $this->currentTab = "Задача";
+        // TODO: handle constraints
+        if (
+            $this->currentTab === "" ||
+            ! in_array($this->currentTab, $this->tabs) ||
+            ($this->currentTab === "Создание команды" && ! $this->canCreateTeam)
+        ) {
+            $this->currentTab = $this->defaultTab;
         }
     }
 };
@@ -50,114 +57,82 @@ new #[Title("Задача")] class extends Component {
     </x-page.button>
 
     <div class="mt-8">
-        <div class="sm:hidden">
-            <label class="sr-only" for="Tab">Tab</label>
-
-            <select class="w-full rounded-md border-gray-200" id="Tab">
-                <option select>Задача</option>
-                <option>Команды</option>
-                <option>Создать команду</option>
-            </select>
-        </div>
-
-        <div class="hidden sm:block">
-            <div class="border-b border-gray-200">
-                <nav class="-mb-px flex gap-6">
-                    <button
-                        :class="$wire.currentTab === 'Задача' ? 'shrink-0 rounded-t-lg border border-gray-300 border-b-white p-3 text-sm font-medium text-sevsu-blue' : 'shrink-0 border border-transparent p-3 text-sm font-medium text-gray-500 hover:text-gray-700'"
-                        wire:click="selectTab('Задача')"
-                    >
-                        Задача
-                    </button>
-
-                    <button
-                        :class="$wire.currentTab === 'Команды' ? 'shrink-0 rounded-t-lg border border-gray-300 border-b-white p-3 text-sm font-medium text-sevsu-blue' : 'shrink-0 border border-transparent p-3 text-sm font-medium text-gray-500 hover:text-gray-700'"
-                        wire:click="selectTab('Команды')"
-                    >
-                        Команды
-                    </button>
-
-                    @if ($canCreateTeam)
-                        <button
-                            :class="$wire.currentTab === 'Создание команды' ? 'shrink-0 rounded-t-lg border border-gray-300 border-b-white p-3 text-sm font-medium text-sevsu-blue' : 'shrink-0 border border-transparent p-3 text-sm font-medium text-gray-500 hover:text-gray-700'"
-                            wire:click="selectTab('Создание команды')"
-                        >
-                            Создать команду
-                        </button>
-                     @endif
-                </nav>
-            </div>
-        </div>
+        <livewire:components.tabs
+            :tabs="$tabs"
+            :currentTab="$currentTab"
+            :defaultTab="$defaultTab"
+            :constraints="['Задача' => true, 'Команды' => true, 'Создание команды' => $canCreateTeam]"
+        />
     </div>
 
     <div
         class="flex flex-col gap-2 overflow-hidden border border-t-0 border-gray-300 bg-sevsu-white px-6 py-4"
     >
-        <section x-show="$wire.currentTab === 'Задача'">
-            <x-page.heading>Информация о задаче</x-page.heading>
-            <x-description-list.root>
-                <x-description-list.item
-                    term="Дисциплина"
-                    :description="$this->flow['flow_name']"
-                />
-                <x-description-list.item
-                    term="Название задачи"
-                    :description="$this->task['task_name']"
-                />
-                <x-description-list.item term="Тэги">
-                    <x-slot:description>
-                        <x-card.tags
-                            :tags="$this->getTaskTags($this->task['id'])"
-                        />
-                    </x-slot>
-                </x-description-list.item>
-                <x-description-list.item
-                    term="Заказчик"
-                    :description="$this->task['customer']"
-                />
-                <x-description-list.item
-                    term="Описание задачи"
-                    :description="$this->task['task_description']"
-                />
-                <x-description-list.item
-                    term="Взять задачу до"
-                    :description="$this->flow['take_before']"
-                />
-                <x-description-list.item
-                    term="Завершить задачу до"
-                    :description="$this->flow['finish_before']"
-                />
-                <x-description-list.item
-                    term="Максимум человек в команде"
-                    :description="$this->flow['max_team_size']"
-                />
-                <x-description-list.item
-                    term="Максимальное количество команд"
-                    :description="$this->task['max_projects']"
-                />
-            </x-description-list.root>
-        </section>
-
-        <section x-show="$wire.currentTab === 'Команды'">
-            <x-page.heading>Команды, выбравшие данную задачу</x-page.heading>
-            <div>
-                @if (count($this->teams) === 0)
-                    <h2 class="text-md py-6">
-                        Нет команд, выбравших эту задачу
-                    </h2>
-                @else
-                    <div class="space-y-8 py-6">
-                        <livewire:components.team-card-list
-                            :teams="$teams"
-                            :flow="$flow"
-                        />
-                    </div>
-                @endif
-            </div>
-        </section>
-
-        @if ($this->canCreateTeam)
-            <section x-show="$wire.currentTab === 'Создание команды'">
+        @if ($currentTab === 'Задача')
+            <section>
+                <x-page.heading>Информация о задаче</x-page.heading>
+                <x-description-list.root>
+                    <x-description-list.item
+                        term="Дисциплина"
+                        :description="$this->flow['flow_name']"
+                    />
+                    <x-description-list.item
+                        term="Название задачи"
+                        :description="$this->task['task_name']"
+                    />
+                    <x-description-list.item term="Тэги">
+                        <x-slot:description>
+                            <x-card.tags
+                                :tags="$this->getTaskTags($this->task['id'])"
+                            />
+                        </x-slot>
+                    </x-description-list.item>
+                    <x-description-list.item
+                        term="Заказчик"
+                        :description="$this->task['customer']"
+                    />
+                    <x-description-list.item
+                        term="Описание задачи"
+                        :description="$this->task['task_description']"
+                    />
+                    <x-description-list.item
+                        term="Взять задачу до"
+                        :description="$this->flow['take_before']"
+                    />
+                    <x-description-list.item
+                        term="Завершить задачу до"
+                        :description="$this->flow['finish_before']"
+                    />
+                    <x-description-list.item
+                        term="Максимум человек в команде"
+                        :description="$this->flow['max_team_size']"
+                    />
+                    <x-description-list.item
+                        term="Максимальное количество команд"
+                        :description="$this->task['max_projects']"
+                    />
+                </x-description-list.root>
+            </section>
+        @elseif ($currentTab === 'Команды')
+            <section>
+                <x-page.heading>Команды, выбравшие данную задачу</x-page.heading>
+                <div>
+                    @if (count($this->teams) === 0)
+                        <h2 class="text-md py-6">
+                            Нет команд, выбравших эту задачу
+                        </h2>
+                    @else
+                        <div class="space-y-8 py-6">
+                            <livewire:components.team-card-list
+                                :teams="$teams"
+                                :flow="$flow"
+                            />
+                        </div>
+                    @endif
+                </div>
+            </section>
+        @elseif ($currentTab === 'Создание команды')
+            <section>
                 <x-page.heading>Форма создания команды</x-page.heading>
                 <livewire:components.team-form :task="$task" :flow="$flow" />
             </section>
