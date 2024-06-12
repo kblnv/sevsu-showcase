9e33ad18e2a1897dc91ad689a97fd1395a49880e
Andrey Ostapovich <artvik48@gmail.com>
2024-06-12 14:33:41 +0300

    Таблица команды вынесена в отдельный компонент

--- resources/views/livewire/pages/teams/show.blade.php
+++ resources/views/livewire/pages/teams/show.blade.php
@@ -80,42 +80,10 @@ new #[Title("Задача")] class extends Component {
                 class="mt-6 overflow-x-auto rounded-lg border border-gray-300 text-sm shadow-sm shadow-gray-300"
                 x-cloak
             >
-                <table class="min-w-full divide-y-2 divide-gray-300 bg-white">
-                    <thead>
-                        <tr>
-                            <td class="px-4 py-2 font-myriad-bold">№</td>
-                            <td class="px-4 py-2 font-myriad-bold">ФИО</td>
-                            <td class="px-4 py-2 font-myriad-bold">Вакансия</td>
-                            <td class="px-4 py-2 font-myriad-bold">Роль</td>
-                        </tr>
-                    </thead>
-
-                    <tbody class="divide-y divide-gray-300">
-                        @foreach ($members as $key => $member)
-                            @php($fullName = "{$member["second_name"]} {$member["first_name"]} {$member["last_name"]}")
-                            @if ($key % 2 === 0)
-                                <x-team.member
-                                    :index="$loop->index + 1"
-                                    :fullName="$fullName"
-                                    :vacancy="$member['vacancy_name']"
-                                    :isModerator="$member['is_moderator']"
-                                />
-                            @else
-                                <x-team.member
-                                    class="bg-gray-50"
-                                    :index="$loop->index + 1"
-                                    :fullName="$fullName"
-                                    :vacancy="$member['vacancy_name']"
-                                    :isModerator="$member['is_moderator']"
-                                />
-                            @endif
-                        @endforeach
-                    </tbody>
-                </table>
-                <div class="border-t border-gray-300 px-4 py-2">
-                    Участников:
-                    {{ count($members) }}/{{ $this->flow["max_team_size"] }}
-                </div>
+            <x-team.table
+                :members="$members"
+                :maxTeamMembers="$this->flow['max_team_size']"
+            />
             </div>
         </section>
         <section class="mt-6">
